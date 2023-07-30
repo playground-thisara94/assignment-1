da77094565b9b08f58c3253bbb08568a327d2961
Thisara Rathnapala <thisaratr94@gmail.com>
2023-07-30 10:00:16 +0530

    Complete Task 3

---
+++ Demo3.java
@@ -0,0 +1,11 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        
+        int num1 = 123;
+        int num2 = 456;
+        int num3 = 7890;
+
+        System.out.printf("(\033[34m%d\033[0m) %d-%d\n", num1,num2,num3);
+
+    }
+}
