ca107e06ca274af23065d17a4b6ab2462f9f5d3c
Thisara Rathnapala <thisaratr94@gmail.com>
2023-07-30 10:51:31 +0530

    Complete Task 5

---
+++ Demo5.java
@@ -0,0 +1,13 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        
+        int num1 = 978;
+        int num2 = 3;
+        int num3 = 16;
+        int num4 = 148410;
+        int num5 = 0;
+
+        System.out.printf("ISBN Number : %d-%d-%d-%d-%d\n", num1,num2,num3,num4,num5);
+
+    }
+}
