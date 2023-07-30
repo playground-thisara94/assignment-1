e0a55209d2e919c5effe358511b3e3d97940f281
Thisara Rathnapala <thisaratr94@gmail.com>
2023-07-30 09:44:26 +0530

    Complete Task 1

---
+++ Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        
+        float balance = 123456.7890f;
+
+        System.out.printf("Balance = $%,.2f\n", balance);
+
+    }
+}
